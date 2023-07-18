5d9dff8358bc8f12f6c6491195d0979145213dc3
xiadeyu <1579883916@qq.com>
2023-07-18 10:54:39 +0800

    esm打包，并使其支持chrome 78浏览器内核

--- rollup/es.config.js
+++ rollup/es.config.js
@@ -1,39 +1,36 @@
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+import babel from "@rollup/plugin-babel";
 
 export default {
-  input: './esm/index.js',
-  plugins: [
-    shims(),
-    nodeResolve(),
-    commonjs(),
-    json()
-  ],
+  input: "./esm/index.js",
+  plugins: [shims(), nodeResolve(), commonjs(), json(), terser(), babel()],
   output: {
-    file: './worker.js',
-    format: 'esm'
-  }
+    file: "./worker.js",
+    format: "esm",
+  },
 };
 
 function shims() {
   return {
     resolveId(specifier) {
-      if(specifier.endsWith('perf_hooks.cjs')) {
-        return 'shim:perf_hooks';
+      if (specifier.endsWith("perf_hooks.cjs")) {
+        return "shim:perf_hooks";
       }
-      if(specifier.endsWith('canvas.cjs')) {
-        return 'shim:canvas';
+      if (specifier.endsWith("canvas.cjs")) {
+        return "shim:canvas";
       }
     },
     load(id) {
-      switch(id) {
-        case 'shim:perf_hooks': {
+      switch (id) {
+        case "shim:perf_hooks": {
           return `
             export const performance = globalThis.performance;
           `;
         }
-        case 'shim:canvas': {
+        case "shim:canvas": {
           return `
             class Canvas {
               constructor(width, height) {
@@ -47,6 +44,6 @@ function shims() {
           `;
         }
       }
-    }
-  }
+    },
+  };
 }
